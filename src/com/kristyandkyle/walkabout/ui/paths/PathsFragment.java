2e973b8896c5a440e7f261da872a29932da55ad2
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2012-12-29 22:12:25 -0500

    switched over to using a cursor loader for loading the data into the listview

--- src/com/kristyandkyle/walkabout/ui/paths/PathsFragment.java
+++ src/com/kristyandkyle/walkabout/ui/paths/PathsFragment.java
@@ -4,7 +4,11 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,19 +19,20 @@ import com.actionbarsherlock.app.SherlockListFragment;
 import com.kristyandkyle.walkabout.R;
 import com.kristyandkyle.walkabout.providers.WalkAbout;
 
-public class PathsFragment extends SherlockListFragment {
+public class PathsFragment extends SherlockListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
-	SimpleCursorAdapter mAdapter;
+	private static final int PATHS_LIST_LOADER = 0x01;
+	private SimpleCursorAdapter mAdapter;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-				
-		Cursor pathCursor = null;
-		ContentResolver contentResolver = getActivity().getContentResolver();
-		pathCursor = contentResolver.query(WalkAbout.Paths.PATHS_URI, null, null, null, null);
-		mAdapter = new SimpleCursorAdapter(getSherlockActivity(), android.R.layout.simple_list_item_1, pathCursor, new String[] { WalkAbout.Paths.NAME }, new int[] { android.R.id.text1 }, 0);
 		
+		String[] uiBindFrom = { WalkAbout.Paths.NAME };
+		int[] uiBindTo = { android.R.id.text1 };
+		
+		getActivity().getSupportLoaderManager().initLoader(PATHS_LIST_LOADER, null, this);
+		mAdapter = new SimpleCursorAdapter(getSherlockActivity(), android.R.layout.simple_list_item_1, null, uiBindFrom, uiBindTo, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
 		setListAdapter(mAdapter);
 	}
 
@@ -44,6 +49,22 @@ public class PathsFragment extends SherlockListFragment {
 		Intent intent = new Intent(getSherlockActivity(), PathActivity.class);
 		intent.putExtra("pathName", textView.getText());
 		startActivity(intent);
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		CursorLoader cursorLoader = new CursorLoader(getSherlockActivity(), WalkAbout.Paths.PATHS_URI, null, null, null, null);
+		return cursorLoader;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		mAdapter.swapCursor(cursor);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		mAdapter.swapCursor(null);
 	}
 	
 }
