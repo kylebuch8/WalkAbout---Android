9ee56537c69a5c11a1da90e823bcb2b06fca8a32
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-01-01 10:10:21 -0500

    started using a cursorloader to load the data

--- src/com/kristyandkyle/walkabout/ui/paths/PathActivity.java
+++ src/com/kristyandkyle/walkabout/ui/paths/PathActivity.java
@@ -1,6 +1,8 @@
 package com.kristyandkyle.walkabout.ui.paths;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,9 +11,17 @@ import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 import com.kristyandkyle.walkabout.R;
+import com.kristyandkyle.walkabout.providers.WalkAbout;
 import com.kristyandkyle.walkabout.ui.walk.WalkActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 
-public class PathActivity extends SherlockFragmentActivity {
+public class PathActivity extends SherlockFragmentActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+	
+	private static final int PATH_LIST_LOADER = 0x01;
+	private long mPathId;
+	private TextView mTxtPathName;
 
 	@Override
 	protected void onCreate(Bundle savedInstance) {
@@ -20,8 +30,8 @@ public class PathActivity extends SherlockFragmentActivity {
 		
 		PathFragment pathFragment = (PathFragment) getSupportFragmentManager().findFragmentById(R.id.pathFragment);
 		
-		TextView txtPathName = (TextView) pathFragment.getView().findViewById(R.id.pathFragment_pathName);
-		txtPathName.setText(getIntent().getStringExtra("pathName"));
+		mPathId = getIntent().getLongExtra("pathId", 0);
+		mTxtPathName = (TextView) pathFragment.getView().findViewById(R.id.pathFragment_pathName);
 		
 		Button btnStartWalk = (Button) pathFragment.getView().findViewById(R.id.pathFragment_btnStartWalk);
 		btnStartWalk.setOnClickListener(new View.OnClickListener() {
@@ -29,11 +39,14 @@ public class PathActivity extends SherlockFragmentActivity {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(getApplicationContext(), WalkActivity.class);
+				intent.putExtra("pathId", mPathId);
 				startActivity(intent);
 			}
 		});
 		
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);	
+		
+		getSupportLoaderManager().initLoader(PATH_LIST_LOADER, null, this);
 	}
 	
 	@Override
@@ -48,6 +61,26 @@ public class PathActivity extends SherlockFragmentActivity {
 		}
 	
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		Uri uri = Uri.parse(WalkAbout.Paths.PATHS_URI + "/" + mPathId);
+		CursorLoader cursorLoader = new CursorLoader(this, uri, null, null, null, null);
+		return cursorLoader;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		cursor.moveToPosition(0);
+		
+		mTxtPathName.setText(cursor.getString(cursor.getColumnIndex(WalkAbout.Paths.NAME)));
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
